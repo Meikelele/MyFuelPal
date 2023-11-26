80b10126c9fb726ce45ad6f994668cbb1b737c2c
Michael_Bak <lenovo.bak2002@gmail.com>
2023-11-26 01:35:24 +0100

    kolejna strona singup z podstawowym routingiem

--- public/views/singup.php
+++ public/views/singup.php
@@ -3,11 +3,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>singup</title>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
     <link rel="stylesheet" href="public/css/global.css">
-    <link rel="stylesheet" href="public/css/signup.css">
+    <link rel="stylesheet" href="public/css/login.css">
+    <link rel="stylesheet" href="public/css/singup.css">
+    
+    <title>SingUp</title>
 </head>
 <body>
+    <main class="flex-column-center">
+        <div class="container-text-header">
+            <p>Let's start here</p>
+            <p class="container-subtext">Fill in yur details to begin</p>
+        </div>
+
+       <form class="form">
+            <div class="input-group">
+                <input type="text" class="input">
+                <label class="placeholder">Fullname</label>    
+            </div>
+        </form> 
+
+        <form class="form">
+            <div class="input-group">
+                <input type="text" class="input">
+                <label class="placeholder">E-mail</label>    
+            </div>
+        </form> 
+
+        <form class="form">
+            <div class="input-group">
+                <input type="password" class="input">
+                <label class="placeholder">Password</label>    
+            </div>
+        </form> 
+
+
+        <a href="#">
+        <button class="container-button-singup"><p>Sing up</p></button>
+        </a>
+        
+        <div class="footer">
+            <p>By singing in, I agree with <span class="important">Terms of Use</span> and <span class="important">Privacy Policy</span></p>
+        </div>
+
+        
+
+    </main>
     
+
 </body>
 </html>
+
+<!-- 
+SHIFT + ENTER  wstawienie nowej lini
+SHITD + OPTION + D skopiowanie aktualnej linii w dol
+
+
+-->
