4d58cca11630e2b5294201da3ace9720adac315d
Michael_Bak <lenovo.bak2002@gmail.com>
2024-01-30 15:12:16 +0100

    nav

---
+++ public/views/common/nav.php
@@ -0,0 +1,53 @@
+<div class="base-container">
+    <h2>My<span>Fuel</span>Pal</h2>
+
+    <nav>
+        <div id="myLinks">
+            <ul>
+                <li>
+                    <i class="fa-solid fa-house"></i>
+                    <a href="/dashboard" class="button">Home</a>
+                </li>
+
+                <li>
+                    <i class="fa-solid fa-car"></i>
+                    <a href="/yourcars" class="button">My Cars</a>
+                </li>
+
+                <li>
+                    <i class="fa-solid fa-gas-pump"></i>
+                    <a href="/yourfuelpal" class="button">My Fuelpal</a>
+                </li>
+
+                <li>
+                    <i class="fa-solid fa-user"></i>
+                    <a href="/profile" class="button">Profile</a>
+                </li>
+
+                <li>
+                    <i class="fa-solid fa-gear"></i>
+                    <a href="/settings" class="button">Settings</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<nav class="mobile-menu">
+    <a href="/dashboard">
+        <i class="fa-solid fa-house"></i>
+    </a>
+    <a href="/yourcars">
+        <i class="fa-solid fa-car"></i>
+    </a>
+    <a href="/yourfuelpal">
+        <i class="fa-solid fa-gas-pump"></i>
+    </a>
+    <a href="/profile">
+        <i class="fa-solid fa-user"></i>
+    </a>
+    <a href="/settings">
+        <i class="fa-solid fa-gear"></i>
+    </a>
+
+
+</nav>
