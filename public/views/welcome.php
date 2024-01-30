d232c72e77ec3d3255300c96fac08e0c782a7932
Michael_Bak <lenovo.bak2002@gmail.com>
2024-01-30 15:14:15 +0100

    created welcome

--- public/views/welcome.php
+++ public/views/welcome.php
@@ -3,51 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <!-- ==========================================
-                              Font, images import
-                    ========================================== -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/996b7b3bef.js" crossorigin="anonymous"></script>
-    
+
+    <!-- CZCIONKI -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,600;0,700;0,900;1,400;1,600;1,700&display=swap" rel="stylesheet" />
+
+    <!-- ARKUSZE STYLOW - CSS -->
     <link rel="stylesheet" href="public/css/global.css">
     <link rel="stylesheet" href="public/css/welcome.css">
+
+    <!-- SKRYPTY - JS -->
+    <script src="https://kit.fontawesome.com/996b7b3bef.js" crossorigin="anonymous" defer></script>
+    <script type="text/javascript" src="../public/js/mobile-menu.js" defer></script>
 
     <title>welcome</title>
 </head>
 <body>
-    <main class="flex-column-center">
-        <div class="container-text-myfuelpal">
-            <p>MyFuelPal</p>
-            <p class="container-subtext">Track your refueling history and more</p>
+    <main>
+        <div class="logo">
+            <h2>My<span>Fuel</span>Pal</h2>
         </div>
 
-        <button class="container-button-signup">
-            <p>Sign up</p>
-        </button>
+        <div class="under-logo">
+            <h3>Track your fuel history</h3>
+        </div>
 
-        <div class="container-subtext"><p>Or</p></div>
+        <a href="/register" class="but-singup">
+            Sign up
+        </a>
 
-        <button class="container-button-login">
-            <p>Login</p>
-        </button>
+        <div class="under-logo">
+            <h3>or</h3>
+        </div>
+
+        <a href="/login" class="but-login">
+            Login
+        </a>
 
         <!-- TODO: Facebook api-->
-        <button class="container-button-loginwithfb">
+        <a class="but-login-with-fb">
             <i class="fa-brands fa-facebook"></i>
-            <p>Connect with Facebook</p>
-        </button>
+            Connect with Facebook
+        </a>
 
     </main>
     
 
 </body>
 </html>
-
-<!-- 
-SHIFT + ENTER  wstawienie nowej lini
-SHITD + OPTION + D skopiowanie aktualnej linii w dol
-
-
--->
